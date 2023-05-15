6daa7d2731fe05eed9892794d2c18e4eb53a63b8
Kybik <82117054+kybikn@users.noreply.github.com>
2023-05-15 12:18:46 +0300

    feat: add cors middleware

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,38 @@
+module.exports = (req, res, next) => {
+    // Массив доменов, с которых разрешены кросс-доменные запросы
+    const allowedCors = [
+        'https://mesto-kybikn.staas.ru',
+        'http://mesto-kybikn.staas.ru',
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:4000',
+    ];
+    
+    const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+    // проверяем, что источник запроса есть среди разрешённых
+    if (allowedCors.includes(origin)) {
+      // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Credentials', true);
+    }
+  
+    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  
+    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  
+    // сохраняем список заголовков исходного запроса
+    const requestHeaders = req.headers['access-control-request-headers'];
+  
+    // Если это предварительный запрос, добавляем нужные заголовки
+    if (method === 'OPTIONS') {
+      // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      // разрешаем кросс-доменные запросы с этими заголовками
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      // завершаем обработку запроса и возвращаем результат клиенту
+      return res.end();
+    }
+  
+    next();
+  };
